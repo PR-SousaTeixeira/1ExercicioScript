fb4736dfe5620c17a96243e10cdeb203bf51e9ca
PR-SousaTeixeira <145346276+PR-SousaTeixeira@users.noreply.github.com>
2023-12-02 14:37:35 +0000

    Add files via upload

---
+++ 1 trabalho JavaScript/script.js
@@ -0,0 +1,29 @@
+var name = "Estudantes";
+let idade = 19; 
+
+console.log(name);
+console.log(idade);
+
+let idadeVezes7 = idade * 7;
+console.log(idadeVezes7);
+
+let estudantes = [
+  {
+    name: "Estudante 1",
+    age: 18,
+    isStudent: false
+  },
+  {
+    name: "Estudante 2",
+    age: 19,
+    isStudent: true
+  },
+  {
+    name: "Estudante 3",
+    age: 20,
+    isStudent: true
+  }
+];
+
+console.log(estudantes[0].isStudent);
+console.log(estudantes[0].age);
